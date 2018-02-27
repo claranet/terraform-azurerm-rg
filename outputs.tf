e63e63889d5c8a8eb90af7ab9f91c562f2e1259b
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-02-27 16:58:10 +0100

    TER-227: Update module

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "resourcegroup_name" {
-  value       = "${azurerm_resource_group.basic.name}"
+  value       = "${azurerm_resource_group.main_rg.name}"
   description = "Resource group name"
 }
 
 output "resourcegroup_id" {
-  value       = "${azurerm_resource_group.basic.id}"
+  value       = "${azurerm_resource_group.main_rg.id}"
   description = "Resource group generated id"
 }
 
 output "resourcegroup_location" {
-  value       = "${azurerm_resource_group.basic.location}"
+  value       = "${azurerm_resource_group.main_rg.location}"
   description = "Resource group location (region)"
 }
