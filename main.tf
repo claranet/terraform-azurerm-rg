b8bbb9e20ff9e52a2996d1765fcddfd853bf0a67
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-02-27 16:47:10 +0100

    TER-227: Uses also stack in RG name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "basic" {
-  name     = "rgp.${var.environment}.${var.client_name}"
+resource "azurerm_resource_group" "main_rg" {
+  name     = "rgp.${var.environment}.${var.client_name}.${var.stack}"
   location = "${var.azurerm_region}"
 
   tags {
