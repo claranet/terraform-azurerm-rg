a3fc90a921f1a0c4ecff3304e502394191afb9cc
Jérémy NANCEL <jeremy.nancel@fr.clara.net>
2018-03-20 12:34:32 +0100

    TER-236 Add custom naming for resource group

--- module.tf
+++ module.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "main_rg" {
-  name     = "rgp.${var.environment}.${var.client_name}.${var.stack}"
+  name     = "${coalesce(var.custom_rg_name, "rgp.${var.environment}.${var.client_name}.${var.stack}")}"
   location = "${var.azure_region}"
 
   tags {
