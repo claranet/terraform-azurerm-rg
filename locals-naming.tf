8fc399a2487b14d0bedf4171c68bdb5a2f24f155
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-06-23 16:36:45 +0200

    AZ-515: Use caf naming provider also for legacy name format

--- locals-naming.tf
+++ locals-naming.tf
@@ -1,7 +1,6 @@
 locals {
   # Naming locals/constants
-  name_prefix  = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
-  default_name = lower("${local.name_prefix}${var.stack}-${var.client_name}-${var.environment}")
-
-  rg_name = coalesce(var.custom_rg_name, var.use_caf_naming ? azurecaf_name.rg.result : "${local.default_name}-rg")
+  name_prefix = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  clara_slug  = "rg"
+  rg_name     = coalesce(var.custom_rg_name, azurecaf_name.rg.result)
 }
